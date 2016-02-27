94a31cda0b00c699bfe5ef78234edae8b026af59
danthony1 <tracker346@comcast.net>
2016-02-27 10:29:39 -0500

    Task 3
    in the point class, the x was equal to y.

---
+++ Rectangle/src/edu/towson/cis/cosc442/project2/rectangle/Point.java
@@ -0,0 +1,23 @@
+package edu.towson.cis.cosc442.project2.rectangle;
+
+/**
+ * The Point Class.
+ */
+public class Point {
+	
+	/** x and y coordinates. */
+	public Double x, y;
+	
+	/**
+	 * Instantiates a new point.
+	 *
+	 * @param x the x
+	 * @param y the y
+	 * 
+	 * x was equal to y
+	 */
+	Point(Double x, Double y) {
+		this.x = x;
+		this.y = y;
+	}
+}
